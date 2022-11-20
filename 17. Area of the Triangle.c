e5dfccf8f9c4d801f607d94fd84706f68f85d96b
Shahriar-Rahman-Alvi <116308008+Shahriar-Rahman-Alvi@users.noreply.github.com>
2022-11-20 23:42:41 +0600

    CSE-Lab-Day-2

---
+++ 17. Area of the Triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, c;
+    double semi, area;
+    printf("This program is to calculate area of a triangle.\n");
+    printf("Enter the first side: ");
+    scanf("%d", &a);
+    printf("Enter the second side: ");
+    scanf("%d", &b);
+    printf("Enter the third side: ");
+    scanf("%d", &c);
+    semi = (a + b + c) / 2;
+    area = sqrt(semi * (semi - a) * (semi - b) * (semi - c));
+    printf("Area of the triangle is: %lf", area);
+    return 0;
+}
